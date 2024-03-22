4eb26bd515cb67b5b3b9c5f2c6f443f319e1c899
André Coelho <archanjoandre@yahoo.com.br>
2024-03-22 18:20:36 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'iontodo',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
